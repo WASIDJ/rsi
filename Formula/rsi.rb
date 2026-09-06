87af3120b38a69185b3195c79a90ef46f1a5fb98
Ryou <qaqnoname@163.com>
2026-09-06 22:54:51 +0800

    fix: add sha256 checksum to formula

--- Formula/rsi.rb
+++ Formula/rsi.rb
@@ -1,8 +1,9 @@
 class Rsi < Formula
   desc "Mihomo Manager CLI for Routers and Remote Control"
   homepage "https://github.com/WASIDJ/rsi"
-  url "https://github.com/WASIDJ/rsi.git", branch: "main"
+  url "https://github.com/WASIDJ/rsi/archive/refs/tags/v1.0.0.tar.gz"
   version "1.0.0"
+  sha256 "a0bf68ce7da158867ea4d425e68073b71568bb21c4402dfb449b177c6e419784"
   license "MIT"
 
   depends_on "go" => :build
